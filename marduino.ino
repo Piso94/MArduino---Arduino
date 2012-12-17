760bc0e86ea076d8614d5153a3fe6790bb165cb0
Piso94 <pisoni.luca94@gmail.com>
2012-12-17 19:31:58 +0100

    Update marduino.ino

--- marduino.ino
+++ marduino.ino
@@ -12,10 +12,16 @@ SoftwareSerial BT(2, 3); // Use pins 2 and 3 as a RX and TX virtual
 
 char serialread; // Initialize the variable serialread as char
 char btread; // Initialize the variable btread as char
+char serial; // Initialize the variable serial as char
 
 void setup() {
 BT.begin(9600); // Control Toy-Car
 Serial.begin(9600); // Debug
+
+pinMode(5, OUTPUT); // Set pin 5 as OUTPUT
+pinMode(6, OUTPUT); // Set pin 6 as OUTPUT
+pinMode(10, OUTPUT); // Set pin 10 as OUTPUT
+pinMode(11, OUTPUT); // Set pin 11 as OUTPUT
 
 Serial.println("All Right!"); // Print on Serial
 }
@@ -29,6 +35,42 @@ serialread = Serial.read(); // Write read of Serial on char
 setup(); // Return to setup
 }
 
+if (btread != '') {
+serial = btread;
+} else {
+serial = serialread;
+}
 
+// Left
+if (serial == 'L') {
+  digitalWrite(5, LOW); // Set pin 5 as LOW
+	digitalWrite(6, HIGH); // Set pin 6 as HIGH
+}
+// Right 
+if (serial == 'R') {
+	digitalWrite(5, HIGH); // Set pin 5 as HIGH
+	digitalWrite(6, LOW); // Set pin 6 as LOW
+}
+// Forward
+if (serial == 'F') {
+	digitalWrite(10, LOW); // Set pin 10 as LOW
+	digitalWrite(11, HIGH); // Set pin 11 as HIGH
+}
+// Back
+if (serial == 'B') {
+	digitalWrite(10, HIGH); // Set pin 10 as HIGH
+	digitalWrite(11, LOW); // Set pin 11 as LOW
+}
+// Stop Left-Right
+if (serial == 'D') {
+        digitalWrite(5, HIGH); // Set pin 5 as HIGH
+        digitalWrite(6, HIGH); // Set pin 6 as HIGH
+}
+// Stop Forward-Back
+if (serial == 'P') {
+        digitalWrite(10, HIGH); // Set pin 10 as HIGH
+        digitalWrite(11, HIGH); // Set pin 11 as HIGH
+}
+delay(10); // Wait 10 milliseconds
 }
 
