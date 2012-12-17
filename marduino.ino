e547a2b6ddeea2c770a46b359f5ef51a885d2a74
Piso94 <pisoni.luca94@gmail.com>
2012-12-17 03:07:10 +0100

    Update marduino.ino

--- marduino.ino
+++ marduino.ino
@@ -6,3 +6,29 @@ Project Name: MArduino
 
 */
 
+#include <SoftwareSerial.h> // Import library
+
+SoftwareSerial BT(2, 3); // Use pins 2 and 3 as a RX and TX virtual
+
+char serialread; // Initialize the variable serialread as char
+char btread; // Initialize the variable btread as char
+
+void setup() {
+BT.begin(9600); // Control Toy-Car
+Serial.begin(9600); // Debug
+
+Serial.println("All Right!"); // Print on Serial
+}
+
+void loop() {
+if (BT.available()) { // Check if BT data is available
+btread = BT.read(); // Write read of BT on char
+} else if (Serial.available()) { // Check if Serial data is available
+serialread = Serial.read(); // Write read of Serial on char
+} else { // Otherwise
+setup(); // Return to setup
+}
+
+
+}
+
