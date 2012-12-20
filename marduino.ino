5078943ca4a404de48515e2f08790a28f7654e11
Piso94 <pisoni.luca94@gmail.com>
2012-12-20 04:35:25 +0100

    Fixed the code to work with the new app

--- marduino.ino
+++ marduino.ino
@@ -10,9 +10,9 @@ Project Name: MArduino
 
 SoftwareSerial BT(2, 3); // Use pins 2 and 3 as a RX and TX virtual
 
-char serialread; // Initialize the variable serialread as char
-char btread; // Initialize the variable btread as char
-char serial; // Initialize the variable serial as char
+int serialread; // Initialize the variable serialread as char
+int btread; // Initialize the variable btread as char
+int serial; // Initialize the variable serial as char
 
 void setup() {
 BT.begin(9600); // Control Toy-Car
@@ -23,7 +23,7 @@ pinMode(6, OUTPUT); // Set pin 6 as OUTPUT
 pinMode(10, OUTPUT); // Set pin 10 as OUTPUT
 pinMode(11, OUTPUT); // Set pin 11 as OUTPUT
 
-Serial.println("All Right!"); // Print on Serial
+Serial.println("All Right"); // Print on Serial
 }
 
 void loop() {
@@ -31,46 +31,50 @@ if (BT.available()) { // Check if BT data is available
 btread = BT.read(); // Write read of BT on char
 } else if (Serial.available()) { // Check if Serial data is available
 serialread = Serial.read(); // Write read of Serial on char
-} else { // Otherwise
-setup(); // Return to setup
 }
 
-if (btread != '') {
+if (btread != -1) {
 serial = btread;
 } else {
 serial = serialread;
 }
 
 // Left
-if (serial == 'L') {
+if (serial == 4) {
   	digitalWrite(5, LOW); // Set pin 5 as LOW
 	digitalWrite(6, HIGH); // Set pin 6 as HIGH
+        Serial.println("Left");
 }
 // Right 
-if (serial == 'R') {
+if (serial == 3) {
 	digitalWrite(5, HIGH); // Set pin 5 as HIGH
 	digitalWrite(6, LOW); // Set pin 6 as LOW
+        Serial.println("Right");
 }
 // Forward
-if (serial == 'F') {
+if (serial == 2) {
 	digitalWrite(10, LOW); // Set pin 10 as LOW
 	digitalWrite(11, HIGH); // Set pin 11 as HIGH
+        Serial.println("Forward");
 }
 // Back
-if (serial == 'B') {
+if (serial == 1) {
 	digitalWrite(10, HIGH); // Set pin 10 as HIGH
 	digitalWrite(11, LOW); // Set pin 11 as LOW
+        Serial.println("Back");
 }
 // Stop Left-Right
-if (serial == 'D') {
+if (serial == 6) {
         digitalWrite(5, HIGH); // Set pin 5 as HIGH
         digitalWrite(6, HIGH); // Set pin 6 as HIGH
+        Serial.println("Stop Left-Right");
 }
 // Stop Forward-Back
-if (serial == 'P') {
+if (serial == 5) {
         digitalWrite(10, HIGH); // Set pin 10 as HIGH
         digitalWrite(11, HIGH); // Set pin 11 as HIGH
+        Serial.println("Stop Forward-Back");
 }
 delay(10); // Wait 10 milliseconds
+serial = -1;
 }
-
