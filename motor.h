b01185fcc8d158e420dba9e160f3d3d909788be6
Piso94 <pisoni.luca94@gmail.com>
2013-01-03 03:27:16 -0800

    Create motor.h

---
+++ motor.h
@@ -0,0 +1,26 @@
+/*
+
+Author: Luca Pisoni
+Date: 17/12/2012
+File Name: motor.h
+What it does: Includes basic commands to use the chip
+
+*/
+
+bool init (int fpin, int bpin, int lpin, int rpin) {
+  pinMode (fpin,OUTPUT);
+  pinMode (bpin,OUTPUT);
+  pinMode (lpin,OUTPUT);
+  pinMode (rpin,OUTPUT);
+
+  retrun true;
+}
+
+void command (int pinHIGH, int pinLOW) {
+  digitalWrite(pinHIGH, HIGH);
+  digitalWrite(pinLOW, LOW); 
+}
+
+void 
+
+
